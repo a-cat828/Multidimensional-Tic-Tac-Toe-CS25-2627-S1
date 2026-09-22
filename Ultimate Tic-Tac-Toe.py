7d80219fedaba897da7ac5c43d8713a537978243
a-cat828 <RyckardM017@stemcollegiate.ca>
2026-09-22 10:02:14 -0600

    thing

---
+++ Ultimate Tic-Tac-Toe.py
@@ -0,0 +1,76 @@
+tic_tac_toe_bord_big =[
+    [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+    [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+],
+    ]
+def print_board():
+    print("   0   1   2")
+    print("+-----")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[0][0][0]} | {tic_tac_toe_bord_big[0][0][1]} | {tic_tac_toe_bord_big[0][0][2]} ||| {tic_tac_toe_bord_big[1][0][0]} | {tic_tac_toe_bord_big[1][0][1]} | {tic_tac_toe_bord_big[1][0][1]} ||| {tic_tac_toe_bord_big[2][0][1]} | {tic_tac_toe_bord_big[2][0][1]} | {tic_tac_toe_bord_big[2][0][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[0][1][0]} | {tic_tac_toe_bord_big[0][1][1]} | {tic_tac_toe_bord_big[0][1][2]} ||| {tic_tac_toe_bord_big[1][1][0]} | {tic_tac_toe_bord_big[1][1][1]} | {tic_tac_toe_bord_big[1][1][1]} ||| {tic_tac_toe_bord_big[2][1][1]} | {tic_tac_toe_bord_big[2][1][1]} | {tic_tac_toe_bord_big[2][1][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[0][2][0]} | {tic_tac_toe_bord_big[0][2][1]} | {tic_tac_toe_bord_big[0][2][2]} ||| {tic_tac_toe_bord_big[1][2][0]} | {tic_tac_toe_bord_big[1][2][1]} | {tic_tac_toe_bord_big[1][2][1]} ||| {tic_tac_toe_bord_big[2][2][1]} | {tic_tac_toe_bord_big[2][2][1]} | {tic_tac_toe_bord_big[2][2][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+
+    print(f"|| {tic_tac_toe_bord_big[3][0][0]} | {tic_tac_toe_bord_big[3][0][1]} | {tic_tac_toe_bord_big[3][0][2]} ||| {tic_tac_toe_bord_big[4][0][0]} | {tic_tac_toe_bord_big[4][0][1]} | {tic_tac_toe_bord_big[4][0][1]} ||| {tic_tac_toe_bord_big[5][0][1]} | {tic_tac_toe_bord_big[5][0][1]} | {tic_tac_toe_bord_big[5][0][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[3][1][0]} | {tic_tac_toe_bord_big[3][1][1]} | {tic_tac_toe_bord_big[3][1][2]} ||| {tic_tac_toe_bord_big[4][1][0]} | {tic_tac_toe_bord_big[4][1][1]} | {tic_tac_toe_bord_big[4][1][1]} ||| {tic_tac_toe_bord_big[5][1][1]} | {tic_tac_toe_bord_big[5][1][1]} | {tic_tac_toe_bord_big[5][1][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[3][2][0]} | {tic_tac_toe_bord_big[3][2][1]} | {tic_tac_toe_bord_big[3][2][2]} ||| {tic_tac_toe_bord_big[4][2][0]} | {tic_tac_toe_bord_big[4][2][1]} | {tic_tac_toe_bord_big[4][2][1]} ||| {tic_tac_toe_bord_big[5][2][1]} | {tic_tac_toe_bord_big[5][2][1]} | {tic_tac_toe_bord_big[5][2][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+
+    print(f"|| {tic_tac_toe_bord_big[6][0][0]} | {tic_tac_toe_bord_big[6][0][1]} | {tic_tac_toe_bord_big[6][0][2]} ||| {tic_tac_toe_bord_big[7][0][0]} | {tic_tac_toe_bord_big[7][0][1]} | {tic_tac_toe_bord_big[7][0][1]} ||| {tic_tac_toe_bord_big[8][0][1]} | {tic_tac_toe_bord_big[7][0][1]} | {tic_tac_toe_bord_big[8][0][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[6][1][0]} | {tic_tac_toe_bord_big[6][1][1]} | {tic_tac_toe_bord_big[6][1][2]} ||| {tic_tac_toe_bord_big[7][1][0]} | {tic_tac_toe_bord_big[7][1][1]} | {tic_tac_toe_bord_big[7][1][1]} ||| {tic_tac_toe_bord_big[8][1][1]} | {tic_tac_toe_bord_big[7][1][1]} | {tic_tac_toe_bord_big[8][1][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[6][1][0]} | {tic_tac_toe_bord_big[6][1][1]} | {tic_tac_toe_bord_big[6][1][2]} ||| {tic_tac_toe_bord_big[7][1][0]} | {tic_tac_toe_bord_big[7][1][1]} | {tic_tac_toe_bord_big[7][1][1]} ||| {tic_tac_toe_bord_big[8][1][1]} | {tic_tac_toe_bord_big[7][1][1]} | {tic_tac_toe_bord_big[8][1][1]} ||")
+    print("|+---+---+---+|+---+---+---+|+---+---+---+|")
+    print(f"|| {tic_tac_toe_bord_big[6][2][0]} | {tic_tac_toe_bord_big[6][2][1]} | {tic_tac_toe_bord_big[6][2][2]} ||| {tic_tac_toe_bord_big[7][2][0]} | {tic_tac_toe_bord_big[7][2][1]} | {tic_tac_toe_bord_big[7][2][1]} ||| {tic_tac_toe_bord_big[8][2][1]} | {tic_tac_toe_bord_big[7][2][1]} | {tic_tac_toe_bord_big[8][2][1]} ||")
+
+
